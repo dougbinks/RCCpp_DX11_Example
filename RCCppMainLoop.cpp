0bef63f99955eefb3ea36b35ed91157cf4313cd7
Doug Binks <doug@enkisoftware.com>
2020-01-01 19:22:44 +0000

    Move most of the code over to the RCCppMainLoop now we have the DX pointers.

--- RCCppMainLoop.cpp
+++ RCCppMainLoop.cpp
@@ -11,6 +11,8 @@
 RUNTIME_COMPILER_SOURCEDEPENDENCY_FILE( "imgui/imgui", ".cpp" );
 RUNTIME_COMPILER_SOURCEDEPENDENCY_FILE( "imgui/imgui_widgets", ".cpp" );
 RUNTIME_COMPILER_SOURCEDEPENDENCY_FILE( "imgui/imgui_draw", ".cpp" );
+RUNTIME_COMPILER_SOURCEDEPENDENCY_FILE( "imgui/imgui_demo", ".cpp" );
+
 // D3D11 has a virtual interace, so we only need to include the header and
 // have a system pointer.
 #include <d3d11.h>
@@ -29,6 +31,11 @@ enum InterfaceIDEnumConsoleExample
 
 struct RCCppMainLoop : RCCppMainLoopI, TInterface<IID_IRCCPP_MAIN_LOOP,IObject>
 {
+    // Our state - now members of RCCppMainLoop
+    bool   show_demo_window    = true;
+    bool   show_another_window = false;
+    ImVec4 clear_color         = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
 	RCCppMainLoop()
 	{
 		PerModuleInterface::g_pSystemTable->pRCCppMainLoopI = this;
@@ -43,6 +50,42 @@ struct RCCppMainLoop : RCCppMainLoopI, TInterface<IID_IRCCPP_MAIN_LOOP,IObject>
         ImGui::Begin("RCCppMainLoop Window" );
         ImGui::Text("You can change Window's code at runtime!");
         ImGui::End();
+
+		
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+
+
+        // Rendering
+        ImGui::Render();
+        PerModuleInterface::g_pSystemTable->pd3dDeviceContext->OMSetRenderTargets(1,
+            &PerModuleInterface::g_pSystemTable->pMainRenderTargetView, NULL);
+        PerModuleInterface::g_pSystemTable->pd3dDeviceContext->ClearRenderTargetView(
+            PerModuleInterface::g_pSystemTable->pMainRenderTargetView, (float*)&clear_color);
 
 	}
 };
