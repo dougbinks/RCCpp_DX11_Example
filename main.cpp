6172f3991bc574c4b60a4da8460d7408d6cb2752
Doug Binks <doug@enkisoftware.com>
2020-01-02 11:53:19 +0000

    Refactor - remove data variables already stored in system table

--- main.cpp
+++ main.cpp
@@ -20,13 +20,6 @@
 #include "SystemTable.h"
 #include "RCCppMainLoop.h"
 
-
-// Data
-static ID3D11Device*            g_pd3dDevice = NULL;
-static ID3D11DeviceContext*     g_pd3dDeviceContext = NULL;
-static IDXGISwapChain*          g_pSwapChain = NULL;
-static ID3D11RenderTargetView*  g_mainRenderTargetView = NULL;
-
 // RCC++ Data
 static RuntimeObjectSystem*	    g_pRuntimeObjectSystem;
 static StdioLogSystem           g_Logger;
@@ -77,7 +70,7 @@ int main(int, char**)
 
     // Setup Platform/Renderer bindings
     ImGui_ImplWin32_Init(hwnd);
-    ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
+    ImGui_ImplDX11_Init(g_SystemTable.pd3dDevice, g_SystemTable.pd3dDeviceContext);
 
     // Initialize RCC++
     RCCppInit();
@@ -165,7 +158,7 @@ bool CreateDeviceD3D(HWND hWnd)
     //createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
     D3D_FEATURE_LEVEL featureLevel;
     const D3D_FEATURE_LEVEL featureLevelArray[2] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_0, };
-    if (D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, createDeviceFlags, featureLevelArray, 2, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &featureLevel, &g_pd3dDeviceContext) != S_OK)
+    if (D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, createDeviceFlags, featureLevelArray, 2, D3D11_SDK_VERSION, &sd, &g_SystemTable.pSwapChain, &g_SystemTable.pd3dDevice, &featureLevel, &g_SystemTable.pd3dDeviceContext) != S_OK)
         return false;
 
     CreateRenderTarget();
@@ -175,22 +168,22 @@ bool CreateDeviceD3D(HWND hWnd)
 void CleanupDeviceD3D()
 {
     CleanupRenderTarget();
-    if (g_pSwapChain) { g_pSwapChain->Release(); g_pSwapChain = NULL; }
-    if (g_pd3dDeviceContext) { g_pd3dDeviceContext->Release(); g_pd3dDeviceContext = NULL; }
-    if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = NULL; }
+    if (g_SystemTable.pSwapChain) { g_SystemTable.pSwapChain->Release(); g_SystemTable.pSwapChain = NULL; }
+    if (g_SystemTable.pd3dDeviceContext) { g_SystemTable.pd3dDeviceContext->Release(); g_SystemTable.pd3dDeviceContext = NULL; }
+    if (g_SystemTable.pd3dDevice) { g_SystemTable.pd3dDevice->Release(); g_SystemTable.pd3dDevice = NULL; }
 }
 
 void CreateRenderTarget()
 {
     ID3D11Texture2D* pBackBuffer;
-    g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
-    g_pd3dDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_mainRenderTargetView);
+    g_SystemTable.pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+    g_SystemTable.pd3dDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_SystemTable.pMainRenderTargetView);
     pBackBuffer->Release();
 }
 
 void CleanupRenderTarget()
 {
-    if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = NULL; }
+    if (g_SystemTable.pMainRenderTargetView) { g_SystemTable.pMainRenderTargetView->Release(); g_SystemTable.pMainRenderTargetView = NULL; }
 }
 
 // Win32 message handler
@@ -203,10 +196,10 @@ LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
     switch (msg)
     {
     case WM_SIZE:
-        if (g_pd3dDevice != NULL && wParam != SIZE_MINIMIZED)
+        if (g_SystemTable.pd3dDevice != NULL && wParam != SIZE_MINIMIZED)
         {
             CleanupRenderTarget();
-            g_pSwapChain->ResizeBuffers(0, (UINT)LOWORD(lParam), (UINT)HIWORD(lParam), DXGI_FORMAT_UNKNOWN, 0);
+            g_SystemTable.pSwapChain->ResizeBuffers(0, (UINT)LOWORD(lParam), (UINT)HIWORD(lParam), DXGI_FORMAT_UNKNOWN, 0);
             CreateRenderTarget();
         }
         return 0;
@@ -224,10 +217,10 @@ LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 bool RCCppInit()
 {
     g_SystemTable.pImContext = ImGui::GetCurrentContext();
-    g_SystemTable.pd3dDevice            = g_pd3dDevice;
-    g_SystemTable.pd3dDeviceContext     = g_pd3dDeviceContext;
-    g_SystemTable.pSwapChain            = g_pSwapChain;
-    g_SystemTable.pMainRenderTargetView = g_mainRenderTargetView;
+    g_SystemTable.pd3dDevice            = g_SystemTable.pd3dDevice;
+    g_SystemTable.pd3dDeviceContext     = g_SystemTable.pd3dDeviceContext;
+    g_SystemTable.pSwapChain            = g_SystemTable.pSwapChain;
+    g_SystemTable.pMainRenderTargetView = g_SystemTable.pMainRenderTargetView;
     g_SystemTable.ImGui_ImplDX11_RenderDrawData = ImGui_ImplDX11_RenderDrawData;
 
     g_pRuntimeObjectSystem = new RuntimeObjectSystem;
